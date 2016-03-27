d84d310fab7c577103923a18eefb7a7a6d66a456
Manuel Aristaran <jazzido@jazzido.com>
2016-03-27 19:12:05 -0400

    Cube.to_h

--- lib/mondrian_rest/mondrian_ext.rb
+++ lib/mondrian_rest/mondrian_ext.rb
@@ -1,5 +1,57 @@
 module Mondrian
   module OLAP
+
+    class Cube
+      def to_h
+        # gather dimensions
+        dimensions = self.dimensions
+                     .find_all { |d| d.dimension_type != :measures }
+                     .map { |d|
+          {
+            :name => d.name,
+            :caption => d.caption,
+            :type => d.dimension_type,
+            :annotations => d.annotations,
+            :hierarchies => d.hierarchies.map { |h|
+              {
+                :name => h.name,
+                :has_all => h.has_all?,
+                :all_member_name => h.all_member_name,
+                :levels => h.levels.map { |l|
+                  {
+                    :name => l.name,
+                    :full_name => l.full_name,
+                    :caption => l.caption
+                  }
+                }
+              }
+            }
+          }
+        }
+
+        return {
+          :name => self.name,
+          :annotations => self.annotations,
+          :dimensions => dimensions,
+          :measures => self.dimensions
+                      .find(&:measures?)
+                      .hierarchy
+                      .levels.first
+                      .members
+                      .find_all(&:visible?)
+                      .map do |m|
+            {
+              :name => m.name,
+              :caption => m.caption,
+              :annotations => m.annotations,
+              :full_name => m.full_name
+            }
+          end
+        }
+      end
+
+    end
+
     class Dimension
       def to_h
         {
