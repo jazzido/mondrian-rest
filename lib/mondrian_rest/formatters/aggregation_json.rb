bcee4930650f01c8157f3b1d7f594c21f6d4d7dd
Manuel Aristaran <jazzido@jazzido.com>
2017-11-09 17:34:50 -0300

    cleanly obtain qs parameters from a formatter

--- lib/mondrian_rest/formatters/aggregation_json.rb
+++ lib/mondrian_rest/formatters/aggregation_json.rb
@@ -1,8 +1,9 @@
 module Mondrian::REST::Formatters
   module AggregationJSON
     def self.call(result, env)
-      add_parents = env['rack.request.query_hash']['parents'] == 'true'
-      debug = env['rack.request.query_hash']['debug'] == 'true'
+      params = env['api.endpoint'].params
+      add_parents = params['parents']
+      debug = params['debug']
 
       result.to_h(add_parents, debug).to_json
     end
